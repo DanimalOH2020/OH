224f7079608a233a3214443d5ed0da7b6970a99e
Dan Biscup <dbiscup@hotmail.com>
2020-09-10 08:59:12 -0500

    Test

--- apis/poi/web/Models/Healthcheck.cs
+++ apis/poi/web/Models/Healthcheck.cs
@@ -8,7 +8,7 @@ namespace poi.Models
         public Healthcheck()
         {
             Message = "POI Service Healthcheck";
-            Status = "Healthy8";
+            Status = "Healthy9";
         }
         [Newtonsoft.Json.JsonProperty(PropertyName = "message")]
         public string Message {get;set;}
